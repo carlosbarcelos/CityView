c958f2f6f0e925d70381adc69963ca3c7e6f5f3d
Carlos Barcelos <cbarcelos@wpi.edu>
2017-12-06 15:33:50 -0500

    More Leaflet changes

--- js/leaflet.js
+++ js/leaflet.js
@@ -28,8 +28,27 @@ var streetlightsTile = L.tileLayer('https://api.tiles.mapbox.com/v4/surv-mqp.5lm
   opacity: 0.5
 });
 
+streetlightsTile.setStyle({fillColor :'blue'})
+
+// Contours Layer
+var contorsTile = L.tileLayer('https://api.tiles.mapbox.com/v4/mapbox.mapbox-terrain-v2/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic3Vydi1tcXAiLCJhIjoiY2o5eDNxZHRtN3hlMzJxbGcycm1kdjNkbCJ9.KbJcO99y-sr0o_x4zZMF0g', {
+  layers: 'mapbox.mapbox-terrain-v2',
+  format: 'image/png',
+  transparency: 'true',
+  maxZoom: 17,
+  opacity: 0.5
+});
+
+
+//GEOJSON layer
+// var geojsonLayer = new L.GeoJSON.AJAX("datasets/spatial-static_temporal-static/streetlight.geojson");
+// geojsonLayer.addTo(map);
+
+
 var overlayMaps = {
-  "StreetLights": streetlightsTile
+  "<i class='fa fa-lightbulb-o'></i> <span style='background-color: #579A5D'>StreetLights</span>": streetlightsTile,
+  "<i class='fa fa-lightbulb-o'></i> <span style='background-color: yellow'>Contors</span>": contorsTile
+  // "geojsonLayer": geojsonLayer
 };
 
 //** Construct the map **\\
@@ -43,69 +62,10 @@ var map = L.map('mapid', {
 // Layers control
 L.control.layers(baseMaps, overlayMaps).addTo(map);
 
-// Information control
-var informationControl = L.Control.extend({
-  options: {
-    position: 'topleft'
-  },
-  onAdd: function (map) {
-    var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom');
-    container.style.backgroundImage = '<i class="fa fa-linkedin"></i>';
-    container.style.backgroundColor = 'white';
-    container.style.width = '26px';
-    container.style.height = '26px';
 
-    container.onclick = function(){
-      console.log('buttonClicked');
-    }
-    return container;
-  },
-});
-map.addControl(new informationControl());
-
-// Second control
-var secondControl =  L.Control.extend({
-
-  options: {
-    position: 'topleft'
-  },
-
-  onAdd: function (map) {
-    var container = L.DomUtil.create('input');
-    container.type="button";
-    container.title="No cat";
-    container.value = "42";
-    container.style.width = '26px';
-    container.style.height = '26px';
-    container.onmouseover = function(){
-      container.style.backgroundColor = 'lightgrey';
-    }
-    container.onmouseout = function(){
-      container.style.backgroundColor = 'white';
-    }
-    container.onclick = function(){
-      console.log('buttonClicked');
-    }
-    return container;
-  }
-});
-
-map.addControl(new secondControl());
-
-var helloPopup = L.popup().setContent('Hello World!');
-
+// Information button
+var informationContent = '<p>Hello world!<br />This is a nice popup.</p>';
+var informationPopup = L.popup().setContent(informationContent);
 L.easyButton('fa-info-circle fa-lg', function(btn, map){
-    helloPopup.setLatLng(map.getCenter()).openOn(map);
+    informationPopup.setLatLng(map.getCenter()).openOn(map);
 }).addTo(map);
-
-// //https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}
-// L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic3Vydi1tcXAiLCJhIjoiY2o5eDNxZHRtN3hlMzJxbGcycm1kdjNkbCJ9.KbJcO99y-sr0o_x4zZMF0g', {
-//     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox.streets'
-// }).addTo(map);
-//
-// L.tileLayer('https://api.tiles.mapbox.com/v4/surv-mqp.5lmjns02/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic3Vydi1tcXAiLCJhIjoiY2o5eDNxZHRtN3hlMzJxbGcycm1kdjNkbCJ9.KbJcO99y-sr0o_x4zZMF0g', {
-//     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-//     maxZoom: 18,
-// }).addTo(map);

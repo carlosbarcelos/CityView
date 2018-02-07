f99457bfa4752f8d4539552aea97f83f08cb0fe0
Carlos Barcelos <cbarcelos@wpi.edu>
2018-02-07 11:24:55 -0500

    Completed styling. Waiting on content.

---
+++ js/dataInformation.js
@@ -0,0 +1,17 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Close when click off
+document.getElementById('myModal').onclick = function() {
+  modal.style.display = "none";
+}

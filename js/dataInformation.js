b14505d9d178ecba067a918d908dae52bce74040
Carlos Barcelos <cbarcelos@wpi.edu>
2018-02-08 09:39:58 -0500

    Completed style and content.

--- js/dataInformation.js
+++ js/dataInformation.js
@@ -1,17 +1,30 @@
 // Get the modal
-var modal = document.getElementById('myModal');
+// var modal = document.getElementById('myModal');
+//
+// // Get the image and insert it inside the modal - use its "alt" text as a caption
+// var img = document.getElementById('myImg');
+// var modalImg = document.getElementById("img01");
+// var captionText = document.getElementById("caption");
+// img.onclick = function(){
+//     modal.style.display = "block";
+//     modalImg.src = this.src;
+//     captionText.innerHTML = this.alt;
+// }
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
+function onClick1(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  document.getElementById("caption01").innerHTML = "1";
 }
 
-// Close when click off
-document.getElementById('myModal').onclick = function() {
-  modal.style.display = "none";
+function onClick2(element) {
+  document.getElementById("img02").src = element.src;
+  document.getElementById("modal02").style.display = "block";
+  document.getElementById("caption02").innerHTML = "2";
+}
+
+function onClick3(element) {
+  document.getElementById("img03").src = element.src;
+  document.getElementById("modal03").style.display = "block";
+  document.getElementById("caption03").innerHTML = "3";
 }

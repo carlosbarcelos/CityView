81ca81b5431824fd944d5b83b3638d83f789d0a9
Carlos Barcelos <cbarcelos@wpi.edu>
2018-02-08 09:41:18 -0500

    Small save

--- js/dataInformation.js
+++ js/dataInformation.js
@@ -1,16 +1,3 @@
-// Get the modal
-// var modal = document.getElementById('myModal');
-//
-// // Get the image and insert it inside the modal - use its "alt" text as a caption
-// var img = document.getElementById('myImg');
-// var modalImg = document.getElementById("img01");
-// var captionText = document.getElementById("caption");
-// img.onclick = function(){
-//     modal.style.display = "block";
-//     modalImg.src = this.src;
-//     captionText.innerHTML = this.alt;
-// }
-
 function onClick1(element) {
   document.getElementById("img01").src = element.src;
   document.getElementById("modal01").style.display = "block";
